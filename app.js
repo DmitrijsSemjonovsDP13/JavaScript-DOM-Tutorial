60598969c2694293f027f02b90b126791af93784
DmitrijsSemjonovsDP13 <dmitrijs.semjonovs.dp13@inbox.lv>
2021-03-09 16:24:02 +0200

    Videoklips 8

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const bookList = document.querySelector('book-list')
+const bookList = document.querySelector('#book-list');
 
-console.log('the parent node is:', bookList.parentNode)
-console.log('the parent element is:', bookList.parentElement.parentElement)
+console.log('#book-list next sibling:', bookList.nextSibling);
+console.log('#book-list next element sibling:', bookList.nextElementSibling);
+console.log('#book-list previous sibling:', bookList.previousSibling);
+console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-console.log('bookList.children')
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
